af2a10f758ff9215e4a235ef7b4dc330ff62c56a
MasatoMiyata <myt.masato@gmail.com>
2024-03-17 15:08:10 +0900

    MKMDWの作成

--- pyHASP_library.py
+++ pyHASP_library.py
@@ -150,6 +150,30 @@ def NDATF(IYR,MON,IDY):
     return y
 
 
+def MKMDW(ID, M):
+    """
+    **************** DATING (DAY OF THE WEEK) ****************************
+    *     LATEST REVISION     - 1996.06.15
+    *     ARGUMENTS ID(7,5)   - INPUT   DATE.
+    *     FUNCTION  MKMDW               DAY OF THE WEEK.
+    *     REQ. ROUTINES       - NDATE
+    **********************************************************************
+    """
+
+    KDY = NDATE(ID[1,2],ID[1,3])
+
+    mkmdw = ID[1,4]-1
+
+    if (mkmdw == 0):
+        mkmdw = 7
+    if (mkmdw < 0):
+        mkmdw = 8
+
+    if (M[KDY+192] == 1):
+        mkmdw = 9   
+    
+    return mkmdw
+
 # ***          4.1. CODE NAME RETRIEVAL ********************************
 def RETRIV(LO,QNAM,M):
     """
@@ -729,7 +753,7 @@ def INWD(NUW,IOPWE,NCLM,ICYCL):
     ISTAT = 1
 
     # 周期データには対応していない
-    
+
     return ICYCL,WD,ID,ISTAT
 
 # if __name__ == '__main__':
